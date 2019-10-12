2a4325f8623082521f5d3c60ffe4bad321c1dd28
rodrigovidotti <rodrigo.vidotti4@gmail.com>
2019-10-11 23:12:19 -0300

    added functions with closures + tests

--- direto.rkt
+++ direto.rkt
@@ -1,36 +1,22 @@
 #lang plai-typed
 
-#|
- | Funções simples
- | Um único argumento, que será representado por um symbol
- |
- |#
-
-; Novo tipo, com funções.
-; Precisamos de duas novas entradas: 
-;     - identificador, para argumentos
-;     - aplicação da função
 (define-type ExprC
-  [numC (n : number)]
-  [idC  (s : symbol)]  ; identificador
-  [appC (fun : symbol) (arg : ExprC)] ; aplicação, com o nome da função e o valor do argumento
+  [numC  (n : number)]
+  [idC   (s : symbol)]  ; identificador
   [plusC (l : ExprC) (r : ExprC)]
   [multC (l : ExprC) (r : ExprC)]
-  [divC (l : ExprC) (r : ExprC)]
+  [divC  (l : ExprC) (r : ExprC)]
+  [lamC (arg : symbol) (body : ExprC)] ; nomes não são mais necessários
+  [appC (fun : ExprC) (arg : ExprC)] ; a aplicação recebe uma função
   [ifC   (condição : ExprC) (sim : ExprC) (não : ExprC)]
   )
-
-; definição de função com 1 argumento
-(define-type FunDefC
-  [fdC (name : symbol) (arg : symbol) (body : ExprC)]
-  )
-
 
 ; inclui funções
 (define-type ExprS
   [numS    (n : number)]
   [idS     (s : symbol)] 
-  [appS    (fun : symbol) (arg : ExprS)] 
+  [lamS    (arg : symbol) (body : ExprS)] ; muda de acordo
+  [appS    (fun : ExprS) (arg : ExprS)] 
   [plusS   (l : ExprS) (r : ExprS)]
   [bminusS (l : ExprS) (r : ExprS)]
   [uminusS (e : ExprS)]
@@ -44,8 +30,9 @@
 (define (desugar [as : ExprS]) : ExprC  
   (type-case ExprS as
     [numS    (n) (numC n)]
-    [idS     (s) (idC s)] ; este é fácil
-    [appS    (fun arg) (appC fun (desugar arg))] ; fun é um symbol, não precisa de desugar 
+    [idS     (s) (idC s)]
+    [lamS    (a b)  (lamC a (desugar b))] ; idem
+    [appS    (fun arg) (appC (desugar fun) (desugar arg))]
     [plusS   (l r) (plusC (desugar l) (desugar r))] 
     [multS   (l r) (multC (desugar l) (desugar r))]
     [divS    (l r) (divC (desugar l) (desugar r))]
@@ -55,63 +42,77 @@
     ))
 
 #|
- | O interpretador precisa de uma lista adicional de definições para os símbolos
+ | Closure não tem mais o nome, mas precisa do environment
  |#
+(define-type Value
+  [numV  (n : number)]
+  [closV (arg : symbol) (body : ExprC) (env : Env)])
 
+
+; símbolos devem se associar ao número (ou a Value?)
 (define-type Binding
-      [bind (name : symbol) (val : number)])
+      [bind (name : symbol) (val : Value)])
 
 ; A lista de associações é o environment
 (define-type-alias Env (listof Binding))
 (define mt-env empty)        ; ente pronunciar "mt" em inglês e compare com "empty"
 (define extend-env cons)     ; sorte, cons faz exatamente o que queremos para estender o env
 
+; novos operadores
+(define (num+ [l : Value] [r : Value]) : Value
+    (cond
+        [(and (numV? l) (numV? r))
+             (numV (+ (numV-n l) (numV-n r)))]
+        [else
+             (error 'num+ "Um dos argumentos não é número")]))
 
-; lookup
-(define (lookup [for : symbol] [env : Env]) : number
+(define (num* [l : Value] [r : Value]) : Value
+    (cond
+        [(and (numV? l) (numV? r))
+             (numV (* (numV-n l) (numV-n r)))]
+        [else
+             (error 'num* "Um dos argumentos não é número")]))
+
+(define (num/ [l : Value] [r : Value]) : Value
+    (cond
+        [(and (numV? l) (numV? r))
+             (numV (/ (numV-n l) (numV-n r)))]
+        [else
+             (error 'num/ "Um dos argumentos não é número")]))
+
+; Agora o interpretador!
+(define (interp [a : ExprC] [env : Env]) : Value
+  (type-case ExprC a
+    [numC (n) (numV n)] ; garantir o retorno do tipo esperado
+    [idC  (n) (lookup n env)]
+    [lamC (a b) (closV a b env)] ; definição de função captura o environment
+    [appC (f a)
+          (local ([define f-value (interp f env)]) ; f-value descreve melhor a ideia
+            (interp (closV-body f-value)
+                    (extend-env 
+                        (bind (closV-arg f-value) (interp a env))
+                        (closV-env f-value) ; não mais mt-env
+                    )))]
+    [plusC (l r) (num+ (interp l env) (interp r env))]
+    [multC (l r) (num* (interp l env) (interp r env))]
+    [divC  (l r) (num/ (interp l env) (interp r env))]
+    [ifC (c s n) (if (zero? (numV-n (interp c env))) (interp n env) (interp s env))]
+    ))
+
+; lookup também muda o tipo de retorno
+(define (lookup [for : symbol] [env : Env]) : Value
        (cond
-            [(empty? env) (error 'lookup "name not found")] ; livre (não definida)
+            [(empty? env) (error 'lookup (string-append (symbol->string for) " não foi encontrado"))] ; livre (não definida)
             [else (cond
                   [(symbol=? for (bind-name (first env)))   ; achou!
                                  (bind-val (first env))]
                   [else (lookup for (rest env))])]))        ; vê no resto
 
-; Agora o interpretador!
-
-
-; interp: todas as chamadas recursivas devem levar em conta o environment
-(define (interp [a : ExprC] [env : Env] [fds : (listof FunDefC)]) : number
-  (type-case ExprC a
-    [numC (n) n]
-    [idC (n) (lookup n env)]
-
-    [appC (f a) 
-          (local ([define fd (get-fundef f fds)]) 
-            (interp (fdC-body fd)
-                    (extend-env 
-                        (bind (fdC-arg fd) (interp a env fds))
-                        mt-env)              ; novo environment feito do zero!
-                    fds))]
-    [plusC (l r) (+ (interp l env fds) (interp r env fds))]
-    [multC (l r) (* (interp l env fds) (interp r env fds))]
-    [divC  (l r) (/ (interp l env fds) (interp r env fds))]
-    [ifC (c s n) (if (zero? (interp c env fds)) (interp n env fds) (interp s env fds))]
-    ))
-
-; get-fundef
-(define (get-fundef [n : symbol] [fds : (listof FunDefC)]) : FunDefC
-  (cond
-    [(empty? fds) (error 'get-fundef "referência para função não definida")]
-    [(cons? fds) (cond
-                   [(equal? n (fdC-name (first fds))) (first fds)] ; achou!
-                   [else (get-fundef n (rest fds))] ; procura no resto
-                   )]))
-
-
 ; o parser precisa tratar de chamadas
 (define (parse [s : s-expression]) : ExprS
   (cond
     [(s-exp-number? s) (numS (s-exp->number s))]
+    [(s-exp-symbol? s) (idS (s-exp->symbol s))] ; pode ser um símbolo livre nas definições de função
     [(s-exp-list? s)
      (let ([sl (s-exp->list s)])
        (case (s-exp->symbol (first sl))
@@ -120,23 +121,10 @@
          [(/) (divS  (parse (second sl)) (parse (third sl)))]
          [(-) (bminusS (parse (second sl)) (parse (third sl)))]
          [(~) (uminusS (parse (second sl)))]
-         [(call) (appS (s-exp->symbol (second sl)) (parse (third sl)))]
+         [(func) (lamS (s-exp->symbol (second sl)) (parse (third sl)))] ; definição
+         [(call) (appS (parse (second sl)) (parse (third sl)))]
          [(if) (ifS (parse (second sl)) (parse (third sl)) (parse (fourth sl)))]
          [else (error 'parse "invalid list input")]))]
     [else (error 'parse "invalid input")]))
 
-#|
- | Nesta linguagem, as funções são pré-definidas
- | Vejamos dobro, quadrado e fatorial
- |#
-(define biblioteca (list 
-                    [fdC 'dobro 'x (plusC (idC 'x) (idC 'x))]
-                    [fdC 'quadrado 'y (multC (idC 'y) (idC 'y))]
-                    [fdC 'fatorial 'n (ifC  (idC 'n) 
-						 (multC (appC 'fatorial (plusC (idC 'n) (numC -1))) 
-								(idC 'n))
-						 (numC 1))]
-                    [fdC 'narciso  'narciso (multC (idC 'narciso) (numC 1000))]
-                    ))
-
-(interp (desugar (parse (read))) mt-env biblioteca)
+(interp (desugar (parse (read))) mt-env)
