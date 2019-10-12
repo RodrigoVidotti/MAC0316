0892282c964524e466f86671e6a36b525fb80644
Unknown <joaopedroturri@gmail.com>
2019-10-12 15:19:57 -0300

    Adding variables

--- direto.rkt
+++ direto.rkt
@@ -9,9 +9,12 @@
   [lamC (arg : symbol) (body : ExprC)] ; nomes não são mais necessários
   [appC (fun : ExprC) (arg : ExprC)] ; a aplicação recebe uma função
   [ifC   (condição : ExprC) (sim : ExprC) (não : ExprC)]
+  [seqC    (e1 : ExprC) (e2 : ExprC)]
+  [setC    (var : symbol) (arg : ExprC)]
+  [letC    (name : symbol) (arg : ExprC) (body : ExprC)]
   )
 
-; inclui funções
+; inclui funções e variáveis
 (define-type ExprS
   [numS    (n : number)]
   [idS     (s : symbol)] 
@@ -23,6 +26,9 @@
   [multS   (l : ExprS) (r : ExprS)]
   [divS    (l : ExprS) (r : ExprS)]
   [ifS     (c : ExprS) (s : ExprS) (n : ExprS)]
+  [seqS    (e1 : ExprS) (e2 : ExprS)]
+  [setS    (var : symbol) (arg : ExprS)]
+  [letS    (name : symbol) (arg : ExprS) (body : ExprS)]
   )
 
 ; agora é preciso arrumar desugar interpretador e parser.
@@ -39,7 +45,13 @@
     [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
     [uminusS (e)   (multC (numC -1) (desugar e))]
     [ifS     (c s n) (ifC (desugar c) (desugar s) (desugar n))]
+    [seqS    (e1 e2)            (seqC (desugar e1) (desugar e2))]
+    [setS    (var expr)         (setC  var (desugar expr))]
+    [letS    (n a b)            (letC n (desugar a) (desugar b))]
     ))
+
+; Precisamos de Storage e Locations
+(define-type-alias Location number)
 
 #|
  | Closure não tem mais o nome, mas precisa do environment
@@ -49,14 +61,22 @@
   [closV (arg : symbol) (body : ExprC) (env : Env)])
 
 
-; símbolos devem se associar ao número (ou a Value?)
+; símbolos devem se associar ao location
 (define-type Binding
-      [bind (name : symbol) (val : Value)])
+      [bind (name : symbol) (val : Location)])
 
 ; A lista de associações é o environment
 (define-type-alias Env (listof Binding))
 (define mt-env empty)        ; ente pronunciar "mt" em inglês e compare com "empty"
 (define extend-env cons)     ; sorte, cons faz exatamente o que queremos para estender o env
+
+;Armazenamento
+(define-type Storage
+      [cell (location : Location) (val : Value)])
+(define-type-alias Store (listof Storage))
+
+(define mt-store empty)
+(define override-store cons)
 
 ; novos operadores
 (define (num+ [l : Value] [r : Value]) : Value
@@ -80,6 +100,9 @@
         [else
              (error 'num/ "Um dos argumentos não é número")]))
 
+(define-type Result
+      [v*s (v : Value) (s : Store)])
+
 ; Agora o interpretador!
 (define (interp [a : ExprC] [env : Env]) : Value
   (type-case ExprC a
@@ -97,16 +120,41 @@
     [multC (l r) (num* (interp l env) (interp r env))]
     [divC  (l r) (num/ (interp l env) (interp r env))]
     [ifC (c s n) (if (zero? (numV-n (interp c env))) (interp n env) (interp s env))]
+    [seqC (b1 b2) (begin (interp b1 env) (interp b2 env))] ; No side effect between expressions!
+    [setC (var val) (let ([b (lookup var env)])
+                               (begin (set-box! b (interp val env)) (unbox b)))]
+    [letC (name arg body)
+                   (let* ([new-bind (bind name (box (interp arg env)))]
+                          [new-env (extend-env new-bind env)])
+                     (interp body new-env))]
     ))
 
 ; lookup também muda o tipo de retorno
-(define (lookup [for : symbol] [env : Env]) : Value
+(define (lookup [for : symbol] [env : Env]) : Location
        (cond
             [(empty? env) (error 'lookup (string-append (symbol->string for) " não foi encontrado"))] ; livre (não definida)
             [else (cond
                   [(symbol=? for (bind-name (first env)))   ; achou!
                                  (bind-val (first env))]
                   [else (lookup for (rest env))])]))        ; vê no resto
+
+; fetch é o lookup do store
+(define (fetch [l : Location] [sto : Store]) : Value
+       (cond
+            [(empty? sto) (error 'fetch "posição não encontrada")]
+            [else (cond
+                  [(= l   (cell-location (first sto)))   ; achou!
+                                 (cell-val (first sto))]
+                  [else (fetch l (rest sto))])]))        ; vê no resto
+
+
+;; retorna a próxima localização disponível
+(define new-loc
+   (let ( [ n (box 0)])
+        (lambda () 
+           (begin
+              (set-box! n (+ 1 (unbox n)))
+              (unbox n)))))
 
 ; o parser precisa tratar de chamadas
 (define (parse [s : s-expression]) : ExprS
@@ -124,6 +172,11 @@
          [(func) (lamS (s-exp->symbol (second sl)) (parse (third sl)))] ; definição
          [(call) (appS (parse (second sl)) (parse (third sl)))]
          [(if) (ifS (parse (second sl)) (parse (third sl)) (parse (fourth sl)))]
+         [(seq) (seqS (parse (second sl)) (parse (third sl)))]
+         [(:=) (setS (s-exp->symbol (second sl)) (parse (third sl)))]
+         [(let) (letS (s-exp->symbol (first (s-exp->list (first (s-exp->list (second sl))))))
+                      (parse (second (s-exp->list (first (s-exp->list (second sl))))))
+                      (parse (third sl)))]
          [else (error 'parse "invalid list input")]))]
     [else (error 'parse "invalid input")]))
 
