8356f8b12a6d509c6678217f4ffae53a2a777e0d
rodrigovidotti <rodrigo.vidotti4@gmail.com>
2019-10-11 23:14:33 -0300

    added tests files

---
+++ tests2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Teste 1"
+cat e13
+cat e13 | ./direto
+echo "Teste 2"
+cat e14
+cat e14 | ./direto
+echo "Teste 3"
+cat e15
+cat e15 | ./direto
+echo "Teste 4"
+cat e16
+cat e16 | ./direto
